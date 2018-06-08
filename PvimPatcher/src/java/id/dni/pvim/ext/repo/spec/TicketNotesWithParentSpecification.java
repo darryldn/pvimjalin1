a87d4ac058efed5bcde24b0e29fe2bda3b0d9e1f
darryl.sulistyan <darryl.sulistyan@W7-IDJKT0161.wincor-nixdorf.com>
2018-06-08 12:28:10 +0700

    Add Order by CREATE_TIME asc

--- PvimPatcher/src/java/id/dni/pvim/ext/repo/spec/TicketNotesWithParentSpecification.java
+++ PvimPatcher/src/java/id/dni/pvim/ext/repo/spec/TicketNotesWithParentSpecification.java
@@ -15,12 +15,13 @@ public class TicketNotesWithParentSpecification implements ISqlSpecification {
 
     private static final String SQL_GET_TICKET_NOTES_WITH_PARENT =
             "select TICKET_ID, CREATE_TIME, NOTE from TICKET_RECORD where TICKET_ID in "
-            + "("
+            + "( "
             + "(SELECT TICKET_ID FROM TICKET WHERE TICKET_NUM=?) "
             + "UNION "
-            + "(SELECT PARENT_ID FROM TICKET WHERE TICKET_NUM=? and PARENT_ID IS NOT NULL)"
+            + "(SELECT PARENT_ID FROM TICKET WHERE TICKET_NUM=? and PARENT_ID IS NOT NULL) "
             + ") "
-            + "AND NOTE_SOURCE_FLAG=1";
+            + "AND NOTE_SOURCE_FLAG=1 "
+            + "ORDER BY CREATE_TIME ASC";
     
     private final String ticketNumber;
     
