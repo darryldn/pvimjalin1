821c35d9dd2821e315acc34a61d6d20383cd1806
darryl.sulistyan <darryl.sulistyan@W7-IDJKT0161.wincor-nixdorf.com>
2018-06-13 10:32:12 +0700

    Remove Notification timer

---
+++ PvimTelegramBot/src/java/id/dni/pvim/ext/telegram/web/servlet/InitServlet.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.dni.pvim.ext.telegram.web.servlet;
+
+import id.dni.pvim.ext.telegram.getupdates.bean.TelegramUpdaterBeanLocal;
+import javax.ejb.EJB;
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author darryl
+ */
+public class InitServlet extends HttpServlet {
+
+    @EJB
+    private TelegramUpdaterBeanLocal updaterBean;
+    
+//    @EJB
+//    private TelegramNotificationBeanLocal notifierBean;
+    
+    @Override
+    public void init() {
+        updaterBean.init();
+//        notifierBean.init();
+    }
+    
+}
