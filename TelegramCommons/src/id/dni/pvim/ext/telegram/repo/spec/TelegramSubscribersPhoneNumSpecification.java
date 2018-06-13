64f235302df8179b844698317506380d43c886fa
darryl.sulistyan <darryl.sulistyan@W7-IDJKT0161.wincor-nixdorf.com>
2018-06-13 08:52:03 +0700

    Fix hardcoded sql

---
+++ TelegramCommons/src/id/dni/pvim/ext/telegram/repo/spec/TelegramSubscribersPhoneNumSpecification.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.dni.pvim.ext.telegram.repo.spec;
+
+import id.dni.pvim.ext.repo.db.spec.ISqlSpecification;
+import id.dni.pvim.ext.telegram.repo.db.vo.TelegramSubscriberVo;
+
+/**
+ *
+ * @author darryl.sulistyan
+ */
+public class TelegramSubscribersPhoneNumSpecification implements ISqlSpecification {
+
+    private static final String SQL_FIND_BY_PHONENUM = 
+            new StringBuilder()
+                    .append(" SELECT ").append(TelegramSubscriberVo.FIELD_SUBS_ID).append(", ")
+                                      .append(TelegramSubscriberVo.FIELD_CHAT_ID).append(", ")
+                                      .append(TelegramSubscriberVo.FIELD_PHONE_NUM).append(", ")
+                                      .append(TelegramSubscriberVo.FIELD_LASTUPDATE).append(", ")
+                                      .append(TelegramSubscriberVo.FIELD_PASSKEY).append(" ")
+                    .append(" FROM ")  .append(TelegramSubscriberVo.TABLE_NAME).append(" ")
+                    .append(" WHERE ") .append(TelegramSubscriberVo.FIELD_PHONE_NUM).append("=? ")
+            .toString();
+            //"select * from PVIM_EXT_TELEGRAM_SUBSCRIBERS where phone_num=?";
+    
+    private final String phoneNumber;
+    
+    public TelegramSubscribersPhoneNumSpecification(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    @Override
+    public String toParameterizedSqlQuery() {
+        return SQL_FIND_BY_PHONENUM;
+    }
+
+    @Override
+    public Object[] getSqlParams() {
+        return (Object[])(new String[]{phoneNumber});
+    }
+    
+}
