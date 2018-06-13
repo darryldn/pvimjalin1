c4b9e2cf4893f9dd7ad68284252d10ed16c6b87b
darryl.sulistyan <darryl.sulistyan@W7-IDJKT0161.wincor-nixdorf.com>
2018-06-13 08:55:30 +0700

    Initial commit

---
+++ PvimCommons/src/id/dni/pvim/ext/repo/db/vo/ITableVoFactory.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.dni.pvim.ext.repo.db.vo;
+
+/**
+ *
+ * @author darryl.sulistyan
+ */
+public interface ITableVoFactory {
+    
+    public ITableDescriptorVo create();
+    
+}
