2825111ec00817d50f002cde3efaff294cc905f2
darryl.sulistyan <darryl.sulistyan@W7-IDJKT0161.wincor-nixdorf.com>
2018-06-13 11:20:21 +0700

    Add update feature

--- PvimTelegramBot/src/java/id/dni/pvim/ext/telegram/chatbot/TelegramChatBotBean.java
+++ PvimTelegramBot/src/java/id/dni/pvim/ext/telegram/chatbot/TelegramChatBotBean.java
@@ -6,13 +6,15 @@
 package id.dni.pvim.ext.telegram.chatbot;
 
 import id.dni.pvim.ext.repo.exceptions.PvExtPersistenceException;
-import id.dni.pvim.ext.telegram.commons.MessageSender;
+import id.dni.pvim.ext.telegram.commons.sender.MessageSender;
 import id.dni.pvim.ext.telegram.pojo.TelegramMessageChatPOJO;
 import id.dni.pvim.ext.telegram.pojo.TelegramMessageContentPOJO;
 import id.dni.pvim.ext.telegram.pojo.TelegramMessagePOJO;
 import id.dni.pvim.ext.telegram.repo.ITelegramSuscribersRepository;
 import id.dni.pvim.ext.telegram.repo.TelegramSubscribersRepository;
 import id.dni.pvim.ext.telegram.repo.db.vo.TelegramSubscriberVo;
+import id.dni.pvim.ext.telegram.repo.spec.TelegramSubscribersByChatIDSpec;
+import id.dni.pvim.ext.telegram.repo.spec.TelegramSubscribersPhoneNumSpecification;
 import id.dni.pvim.ext.web.in.Commons;
 import java.util.Random;
 import java.util.logging.Level;
@@ -41,6 +43,31 @@ public class TelegramChatBotBean implements TelegramChatBotBeanLocal {
             sb.append(r.nextInt(10));
         }
         return sb.toString();
+    }
+    
+    private static boolean eq(Object a, Object b) {
+        if (a == null && b == null) {
+            return true;
+        }
+        
+        if (a == null || b == null) {
+            return false;
+        }
+        
+        return a.equals(b);
+    }
+    
+    private static void sendTelegramMessage(
+            long chatID, boolean isSuccess, String successMessage, String failMessage) {
+        
+        if (isSuccess) {
+            MessageSender.sendMessageAndSwallowLogs(chatID, successMessage);
+
+        } else {
+            MessageSender.sendMessageAndSwallowLogs(chatID, failMessage);
+
+        }
+        
     }
     
     @Override
@@ -85,31 +112,58 @@ public class TelegramChatBotBean implements TelegramChatBotBeanLocal {
                     // TODO: Find the phone number in ProView.
                     // If not registered, send error message saying no phone number found
                     
-                    // save chat to DB
-                    TelegramSubscriberVo subscriber = new TelegramSubscriberVo();
-                    subscriber.setChat_id(id);
-                    subscriber.setLastupdate(System.currentTimeMillis());
-                    subscriber.setPhone_num(probablePhoneNum);
-                    subscriber.setSubs_id(Commons.randomUUID());
-                    
-                    String passKey = genPassKey();
-                    subscriber.setPasskey(passKey);
+                    String phoneNum = probablePhoneNum;
+                    long chatID = id;
                     ITelegramSuscribersRepository repos = new TelegramSubscribersRepository();
+                    
                     try {
-                        boolean isSuccess = repos.insert(subscriber);
-                        if (!isSuccess) {
-                            MessageSender.sendMessageAndSwallowLogs(id, "Registration failed. Please try again later");
+                        // modify the code to enforce 1-1 relationship between phone num and chat id
+                        TelegramSubscriberVo existingPhone = repos.querySingleResult(
+                                new TelegramSubscribersPhoneNumSpecification(phoneNum));
+                        if (existingPhone != null) {
+                            if (chatID != existingPhone.getChat_id()) {
+                                // new ChatID is found with same phone number. Update entry in DB
+                                existingPhone.setChat_id(chatID);
+                                existingPhone.setLastupdate(System.currentTimeMillis());
+                                boolean ok = repos.update(existingPhone);
+                                sendTelegramMessage(chatID, ok, "Update chatID successful", 
+                                        "Update chatID failed, try again later");
+                            }
+                            // entry exists in DB, no need to do anything
                             
                         } else {
-                            MessageSender.sendMessageAndSwallowLogs(id, String.format(
-                                    "Registration successful with number %s! Your passKey is %s", 
-                                    probablePhoneNum, passKey));
-                            
+                            existingPhone = repos.querySingleResult(
+                                new TelegramSubscribersByChatIDSpec(chatID));
+                            if (existingPhone != null) {
+                                if (!eq(existingPhone.getPhone_num(), phoneNum)) {
+                                    // new phone Number for existing chatID. Update entry in DB
+                                    existingPhone.setPhone_num(phoneNum);
+                                    existingPhone.setLastupdate(System.currentTimeMillis());
+                                    boolean ok = repos.update(existingPhone);
+                                    sendTelegramMessage(chatID, ok, "Update mobile number successful", 
+                                            "Update mobile number failed, try again later");
+                                }
+                            } else {
+                                // completely new entry
+                                TelegramSubscriberVo newSub = new TelegramSubscriberVo();
+                                newSub.setChat_id(chatID);
+                                newSub.setLastupdate(System.currentTimeMillis());
+                                newSub.setPhone_num(phoneNum);
+                                newSub.setSubs_id(Commons.randomUUID());
+                                newSub.setPasskey(genPassKey());
+                                boolean ok = repos.insert(newSub);
+                                sendTelegramMessage(chatID, ok, 
+                                        String.format(
+                                            "Registration successful with number %s! Your passKey is %s", 
+                                            probablePhoneNum, newSub.getPasskey()), 
+                                        "Registration failed. Please try again later");
+                                
+                            }
                         }
                         
                     } catch (PvExtPersistenceException ex) {
-                        Logger.getLogger(TelegramChatBotBean.class.getName()).log(Level.SEVERE, null, ex);
-                        MessageSender.sendMessageAndSwallowLogs(id, "Registration failed. Error message: " + ex.getMessage());
+                        Logger.getLogger(TelegramChatBotBean.class.getName()).log(Level.SEVERE, "Database error", ex);
+                        MessageSender.sendMessageAndSwallowLogs(id, "Registration failed. Contact system administrator for details");
                         
                     }
                 }
