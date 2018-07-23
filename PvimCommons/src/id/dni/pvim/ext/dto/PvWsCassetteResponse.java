6658b1ac7288ba26d0513bcb8a18d5308bc46cc1
darryl.sulistyan <darryl.sulistyan@W7-IDJKT0161.wincor-nixdorf.com>
2018-07-23 14:50:52 +0700

    Fix issue ClassCastException when calling request DeviceCounter

---
+++ PvimCommons/src/id/dni/pvim/ext/dto/PvWsCassetteResponse.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.dni.pvim.ext.dto;
+
+import id.dni.pvim.ext.web.in.OperationError;
+import java.util.List;
+
+/**
+ *
+ * @author darryl.sulistyan
+ */
+public class PvWsCassetteResponse {
+    private OperationError err;
+    private List<PvWsCassette> response;
+
+    public OperationError getErr() {
+        return err;
+    }
+
+    public void setErr(OperationError err) {
+        this.err = err;
+    }
+
+    public List<PvWsCassette> getResponse() {
+        return response;
+    }
+
+    public void setResponse(List<PvWsCassette> response) {
+        this.response = response;
+    }
+}
