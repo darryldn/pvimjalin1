f3e12f86517df63a41379a1d16e3c2d57454e473
darryl.sulistyan <darryl.sulistyan@W7-IDJKT0161.wincor-nixdorf.com>
2018-08-18 12:01:13 +0700

    Add pagination

--- ConnReader/src/java/springstuff/dao/spec/GetAllComponentStateWithDeviceDescrSpec.java
+++ ConnReader/src/java/springstuff/dao/spec/GetAllComponentStateWithDeviceDescrSpec.java
@@ -5,6 +5,8 @@
  */
 package springstuff.dao.spec;
 
+import id.dni.pvim.ext.repo.db.pagination.IPaginator;
+import id.dni.pvim.ext.repo.db.pagination.Mssql2012Paginator;
 import id.dni.pvim.ext.repo.db.spec.ISqlSpecification;
 import springstuff.model.ComponentStateVo;
 
@@ -28,9 +30,60 @@ public class GetAllComponentStateWithDeviceDescrSpec implements ISqlSpecificatio
 //                    .append(" where ").append(ComponentStateVo.FIELD_COMPONENT).append(" is not null ")
                     .toString();
     
+    private final int pageSize;
+    private final int pageNum;
+    public GetAllComponentStateWithDeviceDescrSpec() {
+        this(-1, -1);
+    }
+    
+    public GetAllComponentStateWithDeviceDescrSpec(int pageSize, int pageNum) {
+        this.pageSize = pageSize;
+        this.pageNum = pageNum;
+    }
+    
+    //select  f.deviceid , p.devicedescr , f.messagetext , f.devicefailstate , f.latitude , f.longitude 
+    //from v_device_fail f, deviceprofile p, (select deviceid from device order by deviceid OFFSET 10 ROWS FETCH NEXT 5 ROWS ONLY) as d where  
+    //f.devicetype = p.devicetype and d.deviceid = f.deviceid;
+    private String getCompleteSql() {
+        String devicePaginatedSql;
+        IPaginator paginator = new Mssql2012Paginator.Builder()
+                .setSql("select deviceid from device")
+                .setOrderByColumn("deviceid")
+                .setPageNumber(pageNum)
+                .setPageSize(pageSize)
+                .build();
+        devicePaginatedSql = paginator.getPaginatedSql();
+        
+        String rawSql = new StringBuilder()
+                .append("select ")
+                    .append("f.").append(ComponentStateVo.FIELD_DEVICEID).append(",")
+                    .append("p.").append(ComponentStateVo.FIELD_DEVICEDESCR).append(",")
+                    .append("f.").append(ComponentStateVo.FIELD_COMPONENT).append(",")
+                    .append("f.").append(ComponentStateVo.FIELD_COMPONENTSTATE).append(",")
+                    .append("f.").append(ComponentStateVo.FIELD_LATITUDE).append(",")
+                    .append("f.").append(ComponentStateVo.FIELD_LONGITUDE).append(" ")
+                .append(" from ")
+                    .append(ComponentStateVo.TABLE_NAME).append(" f,")
+                    .append(" deviceprofile p,")
+                    .append(" (").append(devicePaginatedSql).append(") as d")
+                .append(" where ")
+                    .append("f.devicetype = p.devicetype").append(" and ")
+                    .append("d.deviceid = f.deviceid")
+//                    .append(" where ").append(ComponentStateVo.FIELD_COMPONENT).append(" is not null ")
+                .toString();
+        
+        return rawSql;
+    }
+    
     @Override
     public String toParameterizedSqlQuery() {
-        return SQL;
+        if (this.pageNum > 0 && this.pageSize > 0) {
+            return getCompleteSql();
+            
+        } else {
+            return SQL;
+            
+        }
     }
 
     @Override
